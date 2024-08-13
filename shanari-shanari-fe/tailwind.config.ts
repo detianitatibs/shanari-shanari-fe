9092c4a753102ea857f5e6c81dbd8a48adb59436
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-08-13 14:01:28 +0900

    フォントとテキストサイズの設定を追加

--- shanari-shanari-fe/tailwind.config.ts
+++ shanari-shanari-fe/tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     // 色は"https://tailwindcss.com/docs/customizing-colors#naming-your-colors"を参照する
+    fontFamily: {
+      inter: ["var(--font-inter)", "sans-serif"],
+      noto: ["var(--font-noto)", "sans-serif"],
+    },
+    fontSize: {
+      size1: "32px",
+      size2: "24px",
+      size3: "20px",
+      size4: "16px",
+      size5: "15px",
+      size6: "13px",
+      size7: "10px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
