0fedc2ac7935313964831bc27e17903177e3d6cc
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-08-14 21:22:43 +0900

    マージンを追加

--- shanari-shanari-fe/src/components/atoms/border-horizon/BorderHorizon.tsx
+++ shanari-shanari-fe/src/components/atoms/border-horizon/BorderHorizon.tsx
@@ -1,5 +1,5 @@
 const BorderHorizon = () => {
-  return <hr className="h-2 drop-shadow-md rounded bg-zinc-200"></hr>;
+  return <hr className="h-2 drop-shadow-md rounded bg-zinc-200 mt-2"></hr>;
 };
 
 export default BorderHorizon;
