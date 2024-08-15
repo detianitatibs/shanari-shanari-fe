4e57e8ed800ec77ec533f492b0446f30eb0c6631
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-08-15 15:06:22 +0900

    ロゴを差し替え

--- shanari-shanari-fe/src/components/atoms/logos/Logos.tsx
+++ shanari-shanari-fe/src/components/atoms/logos/Logos.tsx
@@ -9,21 +9,23 @@ const Logos = ({ pattern, onClick }: Props) => {
   if (pattern === "main") {
     return (
       <Image
-        src="/logo_main.png"
+        src="/logo_main.drawio.svg"
         width={860}
         height={214}
         onClick={onClick}
         alt="Main Logo"
+        className="rounded-md"
       />
     );
   } else {
     return (
       <Image
-        src="/logo_header.png"
+        src="/logo_header.drawio.svg"
         width={120}
         height={40}
         onClick={onClick}
         alt="Header Logo"
+        className="rounded-md"
       />
     );
   }
