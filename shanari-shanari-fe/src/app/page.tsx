49a411d1906e17f6530ea3d34c611843d0b6f70a
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-08-16 00:15:12 +0900

    トップページを作成

--- shanari-shanari-fe/src/app/page.tsx
+++ shanari-shanari-fe/src/app/page.tsx
@@ -1,10 +1,31 @@
-import Image from "next/image";
+import Top from "@/components/templates/top/Top";
 
 export default function Home() {
+  // テスト用に一旦準備
+  const array_card = [];
+  const card = {
+    image_path: "/icon.png",
+    title: "これはブログのタイトルです",
+    category: "blog",
+    date: new Date(2024, 10, 10),
+  };
+  // 複数カードを用意する
+  for (let i = 0; i < 3; i++) {
+    array_card.push(card);
+  }
+
+  const obj_tech = {
+    subject_name: "Technologies",
+    cards: array_card,
+  };
+  const obj_blog = {
+    subject_name: "Blog",
+    cards: array_card,
+  };
+
   return (
-    <>
-      <div className="font-noto font-bold text-size1">Title</div>
-      <div className="font-noto text-size5">Subtitile</div>
-    </>
+    <main>
+      <Top obj_tech={obj_tech} obj_blog={obj_blog}></Top>
+    </main>
   );
 }
