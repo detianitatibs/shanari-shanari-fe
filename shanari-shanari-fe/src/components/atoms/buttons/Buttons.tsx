2581a89d7cc0ad41133ebea371d26f5a70c48bb5
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-09-11 00:13:47 +0900

    ボタンの幅を調整

--- shanari-shanari-fe/src/components/atoms/buttons/Buttons.tsx
+++ shanari-shanari-fe/src/components/atoms/buttons/Buttons.tsx
@@ -6,7 +6,7 @@ const Buttons = ({ text }: Props) => {
   return (
     <button
       type="button"
-      className="text-zinc-100 bg-indigo-400 hover:bg-indigo-500 text-size5 rounded-lg p-1"
+      className="text-zinc-100 bg-indigo-400 hover:bg-indigo-500 text-size5 rounded-lg py-1 px-2"
     >
       {text}
     </button>
