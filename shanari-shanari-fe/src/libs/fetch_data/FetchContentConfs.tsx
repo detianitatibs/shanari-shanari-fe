79d432b6035b5364898490bd7ee8db669b906b2a
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-09-13 00:15:22 +0900

    ログレベルを変更

--- shanari-shanari-fe/src/libs/fetch_data/FetchContentConfs.tsx
+++ shanari-shanari-fe/src/libs/fetch_data/FetchContentConfs.tsx
@@ -7,12 +7,12 @@ import ContentConfDetail from "@/types/ContentConfDetail";
 import ContentConf from "@/types/ContentConf";
 
 const FetchContentConfs = (directory_path: string): ContentConfDetail[] => {
-  logger.info(`directory_path: ${directory_path}`);
+  logger.debug(`directory_path: ${directory_path}`);
 
   // コンテンツのパスを取得する
   const contents_path = path.join(directory_path, "contents");
   const array_contents = fs.readdirSync(contents_path);
-  logger.info(`Got Contents: ${array_contents}`);
+  logger.debug(`Got Contents: ${array_contents}`);
 
   // コンテンツごとにContentConf.yamlを読み込む
   const array_obj_contents: ContentConfDetail[] = [];
@@ -30,9 +30,9 @@ const FetchContentConfs = (directory_path: string): ContentConfDetail[] => {
     ) as ContentConf;
     array_obj_contents.push(obj_content.ContentConf);
   });
-  logger.info("Loaded yaml file of ContentConf");
+  logger.debug("Loaded yaml file of ContentConf");
   array_obj_contents.forEach((obj_content) => {
-    logger.info(obj_content);
+    logger.debug({ msg: obj_content });
   });
 
   return array_obj_contents;
