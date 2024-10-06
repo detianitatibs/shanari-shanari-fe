ecdbbba86541aed4be3e48731f8c206c2a3b2b11
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-10-06 11:42:04 +0900

    何らかのプラットフォームからのアクセスのみコンテンツを表示するように修正

--- shanari-shanari-fe/src/app/page.tsx
+++ shanari-shanari-fe/src/app/page.tsx
@@ -5,27 +5,29 @@ import logger from "@/libs/util/logger";
 import { headers } from "next/headers";
 
 const Page = async () => {
-  // 調査用ログ
+  // 何らかのプラットフォームからのアクセスのみコンテンツを表示する (#44対策)
   const platform = headers().get("sec-ch-ua-platform");
   logger.info(`sec-ch-ua-platform: ${platform}`);
+  if (platform != null) {
+    // コンテンツ一覧を取得
+    const array_obj_contents = await FetchContentConfs("public/");
+    logger.info("Got array_obj_contents");
+    logger.info({ msg: array_obj_contents });
 
-  // コンテンツ一覧を取得
-  const array_obj_contents = await FetchContentConfs("public/");
-  logger.info("Got array_obj_contents");
-  logger.info({ msg: array_obj_contents });
-
-  // カード一覧を取得
-  const array_cards = await FetchCardConfs(
-    "public/",
-    array_obj_contents,
-    Number(process.env.TOPCUT_TOP)
-  );
-
-  return (
-    <main>
-      <Top contents={array_cards}></Top>
-    </main>
-  );
+    // カード一覧を取得
+    const array_cards = await FetchCardConfs(
+      "public/",
+      array_obj_contents,
+      Number(process.env.TOPCUT_TOP)
+    );
+    return (
+      <main>
+        <Top contents={array_cards}></Top>
+      </main>
+    );
+  } else {
+    return <main> No Platform </main>;
+  }
 };
 
 // SSRを強制する
