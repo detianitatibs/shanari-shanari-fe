8d7e238a5b7a2147812abe0df60809a9ecd7c80d
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-10-06 10:51:17 +0900

    ブラウザの調査用ログを仕込む

--- shanari-shanari-fe/src/app/page.tsx
+++ shanari-shanari-fe/src/app/page.tsx
@@ -2,8 +2,13 @@ import Top from "@/components/templates/top/Top";
 import FetchContentConfs from "@/libs/fetch_data/FetchContentConfs";
 import FetchCardConfs from "@/libs/fetch_data/FetchCardConfs";
 import logger from "@/libs/util/logger";
+import { headers } from "next/headers";
 
 const Page = async () => {
+  // 調査用ログ
+  const platform = headers().get("sec-ch-ua-platform");
+  console.log(`sec-ch-ua-platform: ${platform}`);
+
   // コンテンツ一覧を取得
   const array_obj_contents = await FetchContentConfs("public/");
   logger.info("Got array_obj_contents");
